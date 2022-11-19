ca53593d3d64c732d4adc66dd0e82f21080eb37c
Tiago Vilela <100055260+Schwedler123@users.noreply.github.com>
2022-11-19 14:33:14 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+import { Cardapio } from './Pages/Cardapio/Cardapio'
+
+export function App() {
+//app sera utilizado para renderizar todos meus componentes
+
+  return (
+    <div>
+      <Cardapio/>
+    </div>
+  )
+}
+
+
---
+++ src/Pages/Cardapio/Cardapio.jsx
@@ -0,0 +1,122 @@
+import './Cardapio.css'
+
+
+export function Cardapio(props) {
+    return (
+        <section className='section-principal' >
+       
+          
+            <div className='container-principal' >
+                <div className='container-header' >
+                    <div className='flex-title-span' >
+                        <h1 className='title' >VILELA <span>COFFE</span></h1>
+                    </div>
+                 
+                    <div>
+                        <p className='content-p' >
+                            Aberto todos os dias. 8h - 21h 
+                        </p>
+                    </div>
+                </div>
+                    <div className='container-desc-title' >
+                        <h2 className='title-desc' >
+                            BRUNCH
+                        </h2>
+                    </div>
+                <div className='global-container-food' >
+                   <div className='container-name-food' >
+                        <h2 className='name-food' >
+                            WAFFLE DOCE
+                        </h2>
+                   </div>
+                   <div className='container-price-food' >
+                        <h2 className='price-food' >
+                            R$37.70
+                        </h2>
+                   </div>
+                </div>
+                <div className='container-content-food' >
+                        <p>
+                          Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo cumque laudantium
+                        </p>
+                </div>
+                <div className='global-container-food' >
+                   <div className='container-name-food' >
+                        <h2 className='name-food' >
+                           ARROZ DOCE
+                        </h2>
+                   </div>
+                   <div className='container-price-food' >
+                        <h2 className='price-food' >
+                            R$37.70
+                        </h2>
+                   </div>
+                </div>
+                <div className='container-content-food' >
+                        <p>
+                          Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo cumque laudantium
+                        </p>
+                </div>
+                <div className='global-container-food' >
+                   <div className='container-name-food' >
+                        <h2 className='name-food' >
+                           MAÇA DOCE
+                        </h2>
+                   </div>
+                   <div className='container-price-food' >
+                        <h2 className='price-food' >
+                            R$37.70
+                        </h2>
+                   </div>
+                </div>
+                <div className='container-content-food' >
+                        <p>
+                          Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo cumque laudantium
+                        </p>
+                </div>
+                    <div className='container-desc-title' >
+                        <h2 className='title-desc' >
+                            BEBIDAS
+                        </h2>
+                    </div>
+                <div className='global-container-food' >
+                   <div className='container-name-food' >
+                        <h2 className='name-food' >
+                           SUCO DE ALFACE
+                        </h2>
+                   </div>
+                   <div className='container-price-food' >
+                        <h2 className='price-food' >
+                            R$37.70
+                        </h2>
+                   </div>
+                </div>
+                <div className='container-content-food' >
+                        <p>
+                          Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo cumque laudantium
+                        </p>
+                </div>
+                <div className='global-container-food' >
+                   <div className='container-name-food' >
+                        <h2 className='name-food' >
+                           VINHO FINO
+                        </h2>
+                   </div>
+                   <div className='container-price-food' >
+                        <h2 className='price-food' >
+                            R$37.70
+                        </h2>
+                   </div>
+                </div>
+                <div className='container-content-food' >
+                        <p>
+                          Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo cumque laudantium
+                        </p>
+                </div>
+                
+            </div>
+       </section>
+            
+       
+    )
+}
